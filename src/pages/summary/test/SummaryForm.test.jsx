a24c7e961bb33e72253b41d26b7dc2386501b92d
Davide <dmolino@twistbioscience.com>
2022-08-15 21:09:47 -0700

    wrote first test and component

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+describe("Testing summary form component", () => {
+  test("Initial conditions", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    expect(checkbox).not.toBeChecked();
+
+    const confirmButton = screen.getByRole("button");
+    expect(confirmButton).toBeDisabled();
+  });
+
+  test("Checkbox enables button on first click and disables on second click", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
+
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeDisabled();
+  });
+
+  //   test("Dropdown", () => {
+  //     render(<SummaryForm />);
+  //     const dropdown = screen.getByRole("select");
+  //     expect(dropdown).toBeInTheDocument();
+  //   });
+});
