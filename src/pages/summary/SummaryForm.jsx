c2eb4062ed00f17970ad1b5d46a8c6f4c2c42be0
Jane Park <janepark@Janes-MBP-2.attlocal.net>
2022-08-29 17:21:49 -0700

    test breaks

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from 'react-bootstrap/Overlay';
+
+// const Example = () => (
+//   <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+//     <Button variant="success">Click me to see</Button>
+//   </OverlayTrigger>
+// );
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>
+        No ice cream will actually be delivered
+      </Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to the <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to 
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
 
   return (
     <Form>
@@ -27,3 +47,4 @@ export default function SummaryForm() {
     </Form>
   );
 }
+ 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 describe("Testing summary form component", () => {
   test("Initial conditions", () => {
     render(<SummaryForm />);
+    
     const checkbox = screen.getByRole("checkbox", {
       name: /terms and conditions/i,
     });
@@ -17,6 +18,7 @@ describe("Testing summary form component", () => {
   test("Checkbox enables button on first click and disables on second click", async () => {
     const user = userEvent.setup();
     render(<SummaryForm />);
+
     const checkbox = screen.getByRole("checkbox", {
       name: /terms and conditions/i,
     });
@@ -31,32 +33,35 @@ describe("Testing summary form component", () => {
     expect(confirmButton).toBeDisabled();
   });
 
-  test("popover responds to hover", () => {
+  test("popover responds to hover", async () => {
     const user = userEvent.setup();
     render(<SummaryForm />);
+
     //popover starts out hidden
     const nullPopover = screen.queryByText(
       /no ice cream will actually be delivered/i
     );
     expect(nullPopover).not.toBeInTheDocument();
+    
     //popover appears upon mouseover of checkbox label
     const termsAndConditions = screen.getByText(
       /terms and conditions/i
     );
-    // const popover = screen.getBy
-    user.hover(termsAndConditions);
+    await user.hover(termsAndConditions);
+
     const popover = screen.getByText(
       /no ice cream will actually be delivered/i
     );
     expect(popover).toBeInTheDocument();
 
     //popover disappears when we mouseout
-    user.unhover(popover);
+    await user.unhover(popover);
     const nullPopoverAgain = screen.queryByText(      
       /no ice cream will actually be delivered/i
     );
     expect(nullPopoverAgain).not.toBeInTheDocument();
   })
+
   // do we even want to test things like dropdowns?
   //   test("Dropdown", () => {
   //     render(<SummaryForm />);
