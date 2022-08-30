f38c5d01c115bcc67623aa3291242522e5d79d2a
Jane Park <janepark@Janes-MBP-2.attlocal.net>
2022-08-29 21:40:45 -0700

    added topping tests;

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap/Row';
+import ToppingOption from './ToppingOption';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
@@ -17,7 +18,7 @@ export default function Options({ optionType }) {
   }, [optionType]);
   
   //replace null with topping option
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const optionItems = items.map(item => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />);
 
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -5,7 +5,8 @@ import Options from '../Options';
 test('displays image for each scoop option from server', async () => {
     render(<Options optionType="scoops"/>);
     //get multiple images, and alt text assumption that names will end with 'scoop'
-    const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+    //async req must use find by, await, and findBy
+    const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
     //check that it returns the right amount of images
     expect(scoopImages).toHaveLength(2);
 
@@ -13,4 +14,14 @@ test('displays image for each scoop option from server', async () => {
     const altText = scoopImages.map(image => image.alt);
     //array and objects need to use toEqual matcher and primitives can use toBe
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('testing toppings', async () => {
+    render(<Options optionType='toppings' />);
+
+    const toppingImages = await screen.findAllByRole('img', { name: /topping$/i });
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map(topping => topping.alt);
+    expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot Fudge topping']);
 });
